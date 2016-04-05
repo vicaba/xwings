e37bf5ede24ce3b481a79e9b92c97b980d902e49
vicaba <vicaba@me.com>
2016-04-05 17:55:34 +0200

    DefaultActionSerializer refactored

--- src/main/scala/edu/url/lasalle/wotgraph/infrastructure/serializers/json/ActionSerializer.scala
+++ src/main/scala/edu/url/lasalle/wotgraph/infrastructure/serializers/json/ActionSerializer.scala
@@ -14,24 +14,18 @@ object ActionSerializer {
 
   val ContextValueKey = "contextValue"
 
-  object ActionReads extends Reads[Action] {
-    override def reads(json: JsValue): JsResult[Action] = {
-      json match {
-        case json: JsObject =>
-          val actionName = (json \ ActionNameKey).as[String]
-          val contextId = (json \ ContextIdKey).as[UUID]
-          val contextValue = (json \ ContextValueKey).as[String]
-          JsSuccess(Action(actionName, contextId, contextValue))
-        case _ => JsError()
-      }
-    }
-  }
+  val actionReads: Reads[Action] = (
+    (__ \ ActionNameKey).read[String] and
+      (__ \ ContextIdKey).read[UUID] and
+      (__ \ ContextValueKey).read[String]
+    ) (Action.apply _)
 
-  object ActionWrites extends OWrites[Action] {
-    override def writes(o: Action): JsObject =
-      Json.obj(ActionNameKey -> o.actionName, ContextIdKey -> o.contextId, ContextValueKey -> o.contextValue)
-  }
+  val actionWrites: OWrites[Action] = (
+    (__ \ ActionNameKey).write[String] and
+      (__ \ ContextIdKey).write[UUID] and
+      (__ \ ContextValueKey).write[String]
+    ) (unlift(Action.unapply))
 
 
-  val actionFormat: OFormat[Action] = OFormat(ActionReads, ActionWrites)
+  val actionFormat: OFormat[Action] = OFormat(actionReads, actionWrites)
 }
