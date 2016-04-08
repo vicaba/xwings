29128d22dd1c161b7e061bd7a86db76c292ae312
vicaba <vicaba@me.com>
2016-04-08 19:54:10 +0200

    Reset of Neo4jThingHelper

--- src/main/scala/edu/url/lasalle/wotgraph/infrastructure/repository/thing/Neo4jThingHelper.scala
+++ src/main/scala/edu/url/lasalle/wotgraph/infrastructure/repository/thing/Neo4jThingHelper.scala
@@ -8,47 +8,10 @@ import scala.collection.JavaConverters._
 
 object Neo4jThingHelper {
 
-  implicit def thingAsNeo4jThingView(t: Thing): Neo4jThing = {
+  implicit def thingAsNeo4jThingView(t: Thing): Neo4jThing =
+    new Neo4jThing(t._id.toString, t.children.map(thingAsNeo4jThingView).asJava, t.id)
 
-    /*    if (t.children.isEmpty) {
-          new Neo4jThing(t._id.toString, Set.empty[Neo4jThing].asJava, t.id)
-        } else {
-          for {
-            e <- t.children
-          } yield {
-            thingAsNeo4jThingView(e)
-          }
-        }*/
-
-    val childrenSet = if (t.children.nonEmpty) {
-      for {
-        e <- t.children
-      } yield {
-        thingAsNeo4jThingView(e)
-      }
-    } else {
-      Set.empty[Neo4jThing]
-    }
-
-    new Neo4jThing(t._id.toString, childrenSet.asJava, t.id)
-  }
-
-  implicit def neo4jThingAsThingView(t: Neo4jThing): Thing = {
-
-    val childrenSetAsScala = t.children.asScala.toSet
-
-    val childrenSet: Set[Thing] = if (childrenSetAsScala.nonEmpty) {
-      for {
-        e <- childrenSetAsScala
-      } yield {
-        neo4jThingAsThingView(e)
-      }
-    } else {
-      Set.empty[Thing]
-    }
-
-    Thing(UUID.fromString(t._id), None, Set.empty, childrenSet, t.getNeo4jId)
-
-  }
+  implicit def neo4jThingAsThingView(t: Neo4jThing): Thing =
+    Thing(UUID.fromString(t._id), None, Set.empty, t.children.asScala.toSet.map(neo4jThingAsThingView), t.getNeo4jId)
 
 }
